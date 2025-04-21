e3cec1474351c4111a01e499db9456eaabd4a970
Michael B. Espeña <espena.michael.pub@gmail.com>
2025-04-21 22:08:24 +0800

    minor refactor for OCaml and F# examples

--- ch01/example-1.1.1.1/Main.fsx
+++ ch01/example-1.1.1.1/Main.fsx
@@ -1,7 +1,6 @@
-let ans1 = [1..10] |> List.reduce (*)
-let ans2 = [1..10] |> List.fold (*) 1
-
-
 let () =
+    let ans1 = [ 1..10 ] |> List.reduce (*)
+    let ans2 = [ 1..10 ] |> List.fold (*) 1
+
     printfn "%d" ans1
     printfn "%d" ans2
--- ch01/example-1.1.1.1/main.ml
+++ ch01/example-1.1.1.1/main.ml
@@ -1,6 +1,6 @@
-let ans1 = List.init 10 (fun i -> i + 1) |> List.fold_left ( * ) 1
-let ans2 = List.init 10 (fun i -> i + 1) |> List.fold_left ( * ) 1
-
 let () =
-  Printf.printf "%d\n" ans1;
-  Printf.printf "%d\n" ans2;
+  let ans1 = List.init 10 (fun i -> i + 1) |> List.fold_left ( * ) 1 in
+  let ans2 = List.init 10 (fun i -> i + 1) |> List.fold_left ( * ) 1 in
+
+  print_int ans1;
+  print_int ans2
